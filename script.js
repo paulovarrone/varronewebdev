0497ffcadfcd9e3c4be9230dc6aac4e7edd34507
Paulo Varrone <paulorobertovarrone@gmail.com>
2023-05-05 20:59:34 -0300

    menu scroll slide up

--- script.js
+++ script.js
@@ -1,56 +1,21 @@
-function mostrarEsconder() {
-  let toggle = this.nextElementSibling;
-  let menuHome = document.getElementById("menuHome");
+let menuHome = document.getElementById("menuHome");
+let menuToggle = document.querySelector('.toggle');
 
-  if (menuHome.style.display == "flex" || $(menuHome).is(":visible")) {
+menuToggle.onclick = function() {
+  menuToggle.classList.toggle('active');
+  if ($(menuHome).is(":visible")) {
     $(menuHome).slideUp();
   } else {
     $(menuHome).slideDown();
   }
-}
+};
 
-
-/*
-// open and close menu
-function mostrarEsconder() {
- let toggle = this.nextElementSibling;
-
- if (menuHome.style.display == "flex") {
-    menuHome.style.display = "none";
- } else {
-    menuHome.style.display = "flex";
+window.addEventListener('scroll', function() {
+  if ($(menuHome).is(":visible")) {
+    $(menuHome).slideUp();
+    menuToggle.classList.remove('active');
   }
-
-
-}*/
-
-
-
-// background menu hamburguer and close(X)
-let menuToggle = document.querySelector('.toggle');
-    menuToggle.onclick = function() {
-    menuToggle.classList.toggle('active')
-      
-}    
-
-
-
-
-
-
-
-
-// add an eventListener scroll
-window.addEventListener("scroll", function() {
-  // hide menu and red (x) when scroll
-  menuHome.style.display = "none";
-  menuToggle.classList.remove('active');
 });
-
-let toggleElements = document.querySelectorAll('.toggle');
-    for (let i = 0; i < toggleElements.length; i++) {
-    toggleElements[i].addEventListener('click', mostrarEsconder);
-}
 
 
 
